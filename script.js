74d5b8093953c709170f39ca649fb831584e0e31
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-01-25 14:29:43 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const CryptoJS = require("crypto-js");
+
+const key = CryptoJS.enc.Hex.parse("0123456789abcdef0123456789abcdef");
+const ciphertext = CryptoJS.enc.Hex.parse("6bc1bee22e409f96e93d7e117393172a");
+
+const plaintext = CryptoJS.AES.decrypt(ciphertext, key, {
+    mode: CryptoJS.mode.ECB,
+    padding: CryptoJS.pad.NoPadding
+});
+
+console.log(plaintext.toString(CryptoJS.enc.Utf8));
